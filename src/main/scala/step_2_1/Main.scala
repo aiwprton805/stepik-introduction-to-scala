678cf3f959a4be37c1f0bed5ac84557910362f04
aiwprton805 <aiwprton805@mail.ru>
2019-11-12 00:33:46 +0300

    add task description

--- src/main/scala/step_2_1/Main.scala
+++ src/main/scala/step_2_1/Main.scala
@@ -2,6 +2,17 @@ package step_2_1
 
 import scala.io.StdIn.readLine
 
+/*
+Считайте со входного потока имя (строку) и напечатайте "Hi, *считанное имя*!" Все нужные библиотеки уже подключены,
+просто напишите решение!
+
+Sample Input:
+Oleg
+
+Sample Output:
+Hi, Oleg!
+*/
+
 object Main {
   def main(args: Array[String]): Unit = {
     val name = readLine()
