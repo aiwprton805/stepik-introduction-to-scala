81e213be1bf069211dd8b50c0b9a909cb4bd591a
aiwprton805 <aiwprton805@mail.ru>
2019-11-12 00:17:22 +0300

    init commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "stepik"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/step_2_1/Main.scala
@@ -0,0 +1,10 @@
+package step_2_1
+
+import scala.io.StdIn.readLine
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val name = readLine()
+    println("Hi, " + name + "!")
+  }
+}
